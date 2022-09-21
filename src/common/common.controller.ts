597a08297163f490dc1efb91e0be841d4c8d6a97
Nati82 <ashenafinati82@gmail.com>
2022-09-21 15:40:13 +0300

    added file error handling

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Header,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
-import { createReadStream } from 'fs';
-import { join } from 'path';
+import * as fs from 'fs';
+import * as util from 'util';
 
 @ApiTags('common')
 @Controller('common')
 export class CommonController {
   @ApiQuery({ name: 'file-path' })
+  @Header('Content-Type', 'image/jpeg')
   @Get('images')
-  async getImages(@Query('file-path') filePath :string, @Res() res: any) {
+  async getImages(@Query('file-path') filePath: string, @Res() res: any) {
     const fPath = filePath.slice(1, filePath.length);
-    console.log("filePath", `${process.cwd()}${fPath}`)
-    const file = createReadStream(
-        `${process.cwd()}${fPath}`,
-    );
+
+    // const readStream = util.promisify(fs.createReadStream);
+
+    // readStream(`${process.cwd()}${fPath}`, {}).then((v) => console.log('value', v)).catch((e) => console.log('error', e))
+    const file = fs.createReadStream(`${process.cwd()}${fPath}`);
     file.pipe(res);
+    
+    file.on('error', (e) => { 
+        res.format({'application/json' () { res.status(400).json({message: e.message})}});
+    })
   }
 }
